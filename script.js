2055004950bc164f6fbd5fd05b3320d04d168745
Guilherry Pereira <guilherry.s@hotmail.com>
2025-02-23 19:24:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.querySelector(".menu-icon");
-    const navMenu = document.querySelector(".menu");
-
-    if (menuIcon && navMenu) {
-        menuIcon.addEventListener("click", function () {
-            navMenu.classList.toggle("show"); // Mostra/esconde o menu no mobile
-        });
-    }
-});
+function toggleMenu() {
+    let menu = document.querySelector(".menu-desktop");
+    menu.classList.toggle("active");
+}
